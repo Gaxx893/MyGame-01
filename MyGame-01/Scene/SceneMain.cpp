9bd398f3169ddd4534cdbbe0db8f10028fe519a7
Gaxx <lwt5gy707g@gmail.com>
2023-10-23 14:32:38 +0900

    Revert "何箇所かコメントアウトした"
    This reverts commit eed0f84df08253123177c4c168fcb94cb373a843.

--- MyGame-01/Scene/SceneMain.cpp
+++ MyGame-01/Scene/SceneMain.cpp
@@ -125,24 +125,24 @@ void SceneMain::Draw()
 // 当たり判定確認
 void SceneMain::CheckCollide()
 {
-	//// 地面とプレイヤーの当たり判定
-	//for (auto& pModel : m_pField->GetGreenModel())
-	//{
-	//	// DxLibの関数を利用して当たり判定をとる
-	//	// result変数に当たり判定の情報を格納している
-	//	MV1_COLL_RESULT_POLY_DIM result;	// 当たりデータ
+	// 地面とプレイヤーの当たり判定
+	for (auto& pModel : m_pField->GetGreenModel())
+	{
+		// DxLibの関数を利用して当たり判定をとる
+		// result変数に当たり判定の情報を格納している
+		MV1_COLL_RESULT_POLY_DIM result;	// 当たりデータ
 
-	//	result = MV1CollCheck_Capsule(pModel->GetModelHandle(),		// モデルのハンドルを取得
-	//		pModel->GetColFrameIndex(),	// 当たり判定に使用するアニメーションの特定のフレームインデックスを取得
-	//		m_pPlayer->GetPos(),		// プレイヤーの現在位置を取得
-	//		m_pPlayer->GetLastPos(),	// プレイヤーの前回の位置を取得(当たり判定の精度向上の為)
-	//		m_pPlayer->GetColRadius());	// プレイヤーの当たり判定用カプセルの半径を取得
+		result = MV1CollCheck_Capsule(pModel->GetModelHandle(),		// モデルのハンドルを取得
+			pModel->GetColFrameIndex(),	// 当たり判定に使用するアニメーションの特定のフレームインデックスを取得
+			m_pPlayer->GetPos(),		// プレイヤーの現在位置を取得
+			m_pPlayer->GetLastPos(),	// プレイヤーの前回の位置を取得(当たり判定の精度向上の為)
+			m_pPlayer->GetColRadius());	// プレイヤーの当たり判定用カプセルの半径を取得
 
-	//	//ポリゴンが一つでもあたっていた場合,isHitをtrueにする
-	//	const bool isHit = result.HitNum > 0;
-	//	if (isHit)
-	//	{
-	//		m_pPlayer->SetIsOnField(true);
-	//	}
-	//}
+		//ポリゴンが一つでもあたっていた場合,isHitをtrueにする
+		const bool isHit = result.HitNum > 0;
+		if (isHit)
+		{
+			m_pPlayer->SetIsOnField(true);
+		}
+	}
 }
